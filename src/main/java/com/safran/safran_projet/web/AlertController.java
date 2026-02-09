c6b315345c427eed6057aeacbe3799ba2a1e6877
essola <essolayk@yahoo.fr>
2026-02-09 12:41:37 +0100

    modif de ci.yml

--- src/main/java/com/safran/safran_projet/web/AlertController.java
+++ src/main/java/com/safran/safran_projet/web/AlertController.java
@@ -14,7 +14,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class AlertController {
-
     private final AlertRepository alertRepository;
     private final EngineRepository engineRepository;
 
